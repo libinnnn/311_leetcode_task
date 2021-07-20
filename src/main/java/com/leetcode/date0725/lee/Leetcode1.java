c82ef27bffc6b7e89898174f772a17c1caa628d9
libin3 <libin3@ruijie.com.cn>
2021-07-20 22:52:44 +0800

    Initial commit

---
+++ src/main/java/com/leetcode/date0725/lee/Leetcode1.java
@@ -0,0 +1,16 @@
+package com.leetcode.date0725.lee;
+
+/**
+ * @author libin
+ * @date 2021-07-20 22:45
+ */
+
+/**
+ * 题目描述
+ */
+
+public class Leetcode1 {
+    public static void main(String[] args) {
+        System.out.println("first leetcode");
+    }
+}
