8c28a046000333b8c3a1120cb1d5a2820cf941a0
libin3 <libin3@ruijie.com.cn>
2021-07-25 15:02:15 +0800

    0725

--- src/main/java/com/leetcode/date0725/lee/Leetcode1.java
+++ src/main/java/com/leetcode/date0725/lee/Leetcode1.java
@@ -5,12 +5,49 @@ package com.leetcode.date0725.lee;
  * @date 2021-07-20 22:45
  */
 
+import java.util.HashMap;
+
 /**
- * 题目描述
+ * 给定一种规律 pattern 和一个字符串 str ，判断 str 是否遵循相同的规律。
+ *
+ * 这里的 遵循 指完全匹配，例如， pattern 里的每个字母和字符串 str 中的每个非空单词之间存在着双向连接的对应规律。
  */
+
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        String[] sList = s.split(" ");
+        int sLength = sList.length;
+        int patterLength = pattern.length();
+        HashMap map = new HashMap();
+
+        if(sLength != patterLength){
+            return false;
+        }
+
+        for(int i = 0; i < patterLength; i++){
+            if(!map.containsKey(pattern.charAt(i))){
+                if(map.containsValue(sList[i])){
+                    return false;
+                }
+                map.put(pattern.charAt(i), sList[i]);
+            } else {
+                if(!map.get(pattern.charAt(i)).equals(sList[i])){
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
+
 
 public class Leetcode1 {
     public static void main(String[] args) {
-        System.out.println("first leetcode");
+        String pattern = "abba";
+        String str = "cat cat cat cat";
+        Solution solution = new Solution();
+        boolean result = solution.wordPattern(pattern, str);
+        System.out.println(result);
     }
 }
---
+++ src/main/java/com/leetcode/date0725/lee/Leetcode2.java
@@ -0,0 +1,76 @@
+package com.leetcode.date0725.lee;
+
+/**
+ * @author libin
+ * @date 2021-07-25 12:39
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 给你一个二进制数组 nums ，你需要从中删掉一个元素。
+ *
+ * 请你在删掉元素的结果数组中，返回最长的且只包含 1 的非空子数组的长度。
+ *
+ * 如果不存在这样的子数组，请返回 0 。
+ */
+class Solution2 {
+    public int longestSubarray(int[] nums) {
+        int count = 0;
+        List<Integer> list = new ArrayList<Integer>();
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] == 1){
+                count++;
+            }
+            if(nums[i] == 0){
+                if(count > 0){
+                    list.add(count);
+                }
+                list.add(0);
+                count = 0;
+            }
+            if(i == nums.length-1 && count > 0){
+                list.add(count);
+            }
+        }
+        System.out.println(list);
+
+
+
+        int max = 0;
+        if(list.size() == 1 && list.get(0) == nums.length){
+            for(Integer k: list){
+                max += k;
+            }
+            return max-1;
+        }
+
+        if(list.size() < 4){
+            for(Integer k: list){
+                max += k;
+            }
+            return max;
+        }
+        for(int j = 0; j < list.size(); j++){
+            if(j+2 > list.size()-1){
+                return max;
+            }
+            int temp = list.get(j) + list.get(j+1) + list.get(j+2);
+            if(temp > max){
+                max = temp;
+            }
+        }
+
+        return 0;
+    }
+}
+
+public class Leetcode2 {
+    public static void main(String[] args) {
+        int[] nums = {1,1,1,1,1,1};
+        Solution2 solution = new Solution2();
+        int result = solution.longestSubarray(nums);
+        System.out.println(result);
+    }
+}
