dafe0dd312c0957f2a447d207ac88244856c9613
libin3 <libin3@ruijie.com.cn>
2021-09-05 13:10:27 +0800

    0906

---
+++ src/main/java/com/leetcode/date0905/lee/LengthOfLIS.java
@@ -0,0 +1,54 @@
+package com.leetcode.date0905.lee;
+
+/**
+ * @author libin
+ * @date 2021-08-30 23:18
+ */
+
+/**
+ * 300. 最长递增子序列
+ * 给你一个整数数组 nums ，找到其中最长严格递增子序列的长度。
+ *
+ * 子序列是由数组派生而来的序列，删除（或不删除）数组中的元素而不改变其余元素的顺序。例如，[3,6,2,7] 是数组 [0,3,1,6,2,2,7] 的子序列。
+ *
+ *  
+ * 示例 1：
+ *
+ * 输入：nums = [10,9,2,5,3,7,101,18]
+ * 输出：4
+ * 解释：最长递增子序列是 [2,3,7,101]，因此长度为 4 。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/longest-increasing-subsequence
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+public class LengthOfLIS {
+    public int lengthOfLIS(int[] nums) {
+        int maxLengthNums[] = new int[nums.length];
+        int maxLength = 0;
+        maxLengthNums[0] = 1;
+        for(int i = 0; i < nums.length; i++){
+            int maxValue = 0;
+            for(int j = 0; j < i; j++){
+                if(nums[j] < nums[i]){
+                    maxValue = Math.max(maxLengthNums[j], maxValue);
+                }
+                maxLengthNums[i] = maxValue + 1;
+            }
+            maxLength = Math.max(maxLength, maxLengthNums[i]);
+        }
+        for(int num: maxLengthNums){
+            System.out.println(num);
+
+        }
+        return maxLength;
+    }
+    public static void main(String[] args) {
+        int nums[] = new int[]{10,9,2,5,3,7,101,18};
+        LengthOfLIS lengthOfLIS = new LengthOfLIS();
+        int result = lengthOfLIS.lengthOfLIS(nums);
+        System.out.println("-------");
+        System.out.println(result);
+    }
+}
